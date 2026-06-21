99380365c0b9c0a57dd07d6871cecd64f5660b61
groovy <arturlewandowski79@gmail.com>
2026-06-21 23:54:50 +0200

    Create extension.js

---
+++ extension.js
@@ -0,0 +1,101 @@
+import St from 'gi://St';
+import * as Main from 'resource:///org/gnome/shell/ui/main.js';
+import Meta from 'gi://Meta';
+import Gio from 'gi://Gio';
+
+function getPowerProfile() {  
+    const proc = Gio.Subprocess.new(
+        ['powerprofilesctl', 'get'],
+        Gio.SubprocessFlags.STDOUT_PIPE
+    );
+  
+    const [, stdout] = proc.communicate_utf8(null, null);
+  
+    return stdout.trim();  
+}
+
+export default class BlackScreenExtension {
+  
+  enable() {
+	  this._enabled = true;
+    this._idleMonitor = global.backend.get_core_idle_monitor();
+    this._actor = null;
+    this._idleWatch = 0;
+    this._activeWatch = 0;
+	  this._actors = []
+    this._installIdleWatch();
+    this._previousProfile = getPowerProfile();
+  }
+  
+  disable() {      
+	  this._enabled = false;
+
+    if (this._idleWatch) {
+      this._idleMonitor.remove_watch(this._idleWatch);
+      this._idleWatch = 0;
+    }
+
+    if (this._activeWatch) {
+      this._idleMonitor.remove_watch(this._activeWatch);
+      this._activeWatch = 0;
+    }
+
+    this._hideScreen();
+  }
+
+  _installIdleWatch() {      
+    this._idleWatch = this._idleMonitor.add_idle_watch(1000 * 60 * 10, () => {
+	    if (!this._enabled) return;
+      this._showScreen();
+    });      
+  }
+
+  _showScreen() {
+	  global.display.set_cursor(Meta.Cursor.NONE);
+
+	  const count = global.display.get_n_monitors();
+
+	  for (let i = 0; i < count; i++) {
+
+	    const m = global.display.get_monitor_geometry(i);
+      
+      const actor = new St.Widget({
+	      style: 'background-color: #000000;'
+	    });
+
+	    actor.reactive = true;
+	    actor.set_position(m.x, m.y);
+	    actor.set_size(m.width, m.height);
+
+	    Main.layoutManager.addTopChrome(actor);
+
+	    this._actors.push(actor);
+	  }
+
+    this._activeWatch = this._idleMonitor.add_user_active_watch(() => {
+	    if (!this._enabled) return;
+      this._hideScreen();
+      this._activeWatch = 0;
+      this._installIdleWatch();
+    });
+        
+    Gio.Subprocess.new(
+      ['powerprofilesctl', 'set', 'power-saver'],
+      Gio.SubprocessFlags.NONE
+    );        
+  }
+
+  _hideScreen() {
+    for (const actor of this._actors) actor.destroy();
+
+	  this._actors = [];
+
+	  global.display.set_cursor(Meta.Cursor.DEFAULT);
+	    
+	  Gio.Subprocess.new(
+      ['powerprofilesctl', 'set', this._previousProfile],
+      Gio.SubprocessFlags.NONE
+    );        
+  }
+
+}
